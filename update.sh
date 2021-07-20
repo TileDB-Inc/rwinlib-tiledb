4c3bbf1bc2841a623ef785f101076a6be5764f00
Dirk Eddelbuettel <edd@debian.org>
2021-07-20 07:14:03 -0500

    update script for ucrt artifacts

--- update.sh
+++ update.sh
@@ -4,7 +4,10 @@
 ## aka what may be used for 'r-devel') build: download the two artifacts
 ## to the ~/Downloads/ directory (i.e. the default location on Ubuntu)
 ## eg for 2.2.2 the URL is https://github.com/TileDB-Inc/TileDB/runs/1766128541
-for f in mingw-w64-MINGW32-tiledb.zip mingw-w64-MINGW64-tiledb.zip; do
+##
+## And now also updated for 'ucrt' builds
+##
+for f in mingw-w64-MINGW32-tiledb.zip mingw-w64-MINGW64-tiledb.zip mingw-w64-ucrt64-tiledb.zip; do
     mv -v ~/Downloads/${f} .
     unzip ${f}
     rm ${f}
@@ -21,10 +24,19 @@ rsync -cav mingw32/lib/libtiledbstatic.a lib/i386/libtiledbstatic.a
 rm -rf mingw32
 rm -v mingw-w64-i686-tiledb-2.9000-1-any.pkg.tar.xz
 
+
 tar xaf mingw-w64-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
 rsync -cav mingw64/include/tiledb/ include/tiledb/
 rsync -cav mingw64/lib/libtiledbstatic.a lib/x64/libtiledbstatic.a
 rm -rf mingw64
 rm -v mingw-w64-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
+
+
+tar xaf mingw-w64-ucrt-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
+rsync -cav ucrt64/include/tiledb/ include/tiledb/
+rsync -cav ucrt64/lib/libtiledbstatic.a lib/x64/libtiledbstatic.a
+rm -rf ucrt64
+rm -v mingw-w64-ucrt-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
+
 
 rm .BUILDINFO .MTREE .PKGINFO
