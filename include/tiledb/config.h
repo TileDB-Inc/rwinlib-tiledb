d393f26e45f503642387e6c1def84e8d4ba4c9eb
John Kerl <kerl.john.r@gmail.com>
2025-05-06 19:41:55 +0000

    Update to TileDB Embedded 2.28.0-rc0

--- include/tiledb/config.h
+++ include/tiledb/config.h
@@ -1,13 +1,11 @@
 /**
  * @file   config.h
  *
- * @author Ravi Gaddipati
- *
  * @section LICENSE
  *
  * The MIT License
  *
- * @copyright Copyright (c) 2017-2023 TileDB, Inc.
+ * @copyright Copyright (c) 2017-2024 TileDB, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -429,6 +427,16 @@ class Config {
    *    Which reader to use for sparse global order queries. "refactored"
    *    or "legacy".<br>
    *    **Default**: refactored
+   * - `sm.query.sparse_global_order.preprocess_tile_merge` <br>
+   *    **Experimental for testing purposes, do not use.**<br>
+   *    Performance configuration for sparse global order read queries.
+   *    If nonzero, prior to loading the first tiles, the reader will run
+   *    a preprocessing step to arrange tiles from all fragments in a single
+   *    globally ordered list. This is expected to improve performance when
+   *    there are many fragments or when the distribution in space of the
+   *    tiles amongst the fragments is skewed. The value of the parameter
+   *    specifies the amount of work per parallel task.
+   *    **Default**: "32768"
    * - `sm.query.sparse_unordered_with_dups.reader` <br>
    *    Which reader to use for sparse unordered with dups queries.
    *    "refactored" or "legacy".<br>
@@ -627,6 +635,9 @@ class Config {
    *    The maximum permissible delay between Azure netwwork request retry
    *    attempts, in milliseconds.
    *    **Default**: 60000
+   * - `vfs.gcs.endpoint` <br>
+   *    The GCS endpoint. <br>
+   *    **Default**: ""
    * - `vfs.gcs.project_id` <br>
    *    Set the GCS project ID to create new buckets to. Not required unless you
    *    are going to use the VFS to create buckets. <br>
@@ -846,15 +857,6 @@ class Config {
    *    When set to `true`, the S3 SDK uses a handler that ignores SIGPIPE
    *    signals.
    *    **Default**: "true"
-   * - `vfs.hdfs.name_node_uri` <br>
-   *    Name node for HDFS. <br>
-   *    **Default**: ""
-   * - `vfs.hdfs.username` <br>
-   *    HDFS username. <br>
-   *    **Default**: ""
-   * - `vfs.hdfs.kerb_ticket_cache_path` <br>
-   *    HDFS kerb ticket cache path. <br>
-   *    **Default**: ""
    * - `config.env_var_prefix` <br>
    *    Prefix of environmental variables for reading configuration
    *    parameters. <br>
@@ -928,12 +930,12 @@ class Config {
    *    together with the open array <br>
    *    **Default**: true
    * - `rest.load_enumerations_on_array_open` <br>
-   *    If true, enumerations will be loaded for the latest array schema on
-   *    array open.
+   *    If true, enumerations will be loaded for the latest array schema and
+   *    sent to server together with the open array.
    *    **Default**: false
    * - `rest.load_enumerations_on_array_open_all_schemas` <br>
-   *    If true, enumerations will be loaded for all array schemas on array
-   *    open.
+   *    If true, enumerations will be loaded for all array schemas and sent to
+   *    server together with the open array.
    *    **Default**: false
    * - `rest.use_refactored_array_open` <br>
    *    If true, the new REST routes and APIs for opening an array will be used
